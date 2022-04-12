85b52f3acf954df437e92b4d2135456cfda63b14
Luis Abrogar <luis.abrogar@stonybrook.edu>
2022-04-12 10:08:36 -0400

    first commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.calculator;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavSelect);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new Restaurant()).commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavSelect = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch(item.getItemId()) {
+                case(R.id.restaurant):
+                    fragment = new Restaurant();
+                    break;
+
+                case(R.id.calculator):
+                    fragment = new Calculator();
+                    break;
+                case(R.id.converter):
+                    fragment = new Converter();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+            return true;
+        }
+    };
+
+
+}
